8770a7156e21189ae001b63807f0b76c0c04cc2f
JoFre20 <jontewalter@googlemail.com>
2022-11-16 16:13:38 +0100

    Setup Workspace

---
+++ src/test/java/com/jojo/NumberRecognitionAPI/NumberRecognitionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojo.NumberRecognitionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberRecognitionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
