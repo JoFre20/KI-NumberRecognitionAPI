303621de799ea61ee2d55a9bba12d44106e97e16
JoFre20 <jontewalter@googlemail.com>
2022-11-16 16:31:24 +0100

    Add MNISTUtilis

---
+++ src/main/java/com/jojo/NumberRecognitionAPI/NeuralNetwork/MNISTUtilis/MnistImage.java
@@ -0,0 +1,24 @@
+package com.jojo.NumberRecognitionAPI.NeuralNetwork.MNISTUtilis;
+
+import java.util.ArrayList;
+
+public class MnistImage {
+	
+	private ArrayList<Float> imgdata;
+	private int label;
+	
+	public MnistImage(ArrayList<Float> imgdata, int label) {
+		this.imgdata = imgdata;
+		this.label = label;
+	}
+	
+	public ArrayList<Float> getImgdata() {
+		return imgdata;
+		
+	}
+	
+	public int getLabel() {
+		return label;
+	}
+
+}
